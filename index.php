86b8f70bcacddecab604ed2d4afa74b5c47c56ad
MichaelRAlian <102036024+MichaelRAlian@users.noreply.github.com>
2022-04-09 21:52:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>COUNTRY MUSICS</title>
-        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
-    
-        </head>
-    <body>
-        <div id="wrapper">
-            <div id="header">
-                <h1>COUNTRY MUSICS</h1>
-                <div id="navigation">
-                    <ul>
-                        <li><a href="index.php">HOME</a></li>
-                        <li><a href="origin.html">ORIGIN OF COUNTRY MUSICS</a></li>
-                        <li><a href="examples.html">EXAMPLES OF COUNTRY MUSICS</a></li>
-                        <li><a href="facts.html">FACTS ABOUT COUNTRY MUSICS</a></li> 
-                    </ul>       
-        </div>
-        </div>
-        <div id="content">
-            <div id="content-wrapper">
-                <div id="about-text">
-                   <h1>COUNTRY MUSICS</h1>
-                    <p>Country is a genre of popular music that originated with blues, church music such as Southern gospel and spirituals, old-time, and American folk music forms including Appalachian, Cajun, Creole, and the cowboy Western music styles of New Mexico, Red Dirt, Tejano, and Texas country.</p> 
-                
-                </div>
-            <div id="about-image">
-                <img src="country.jpg" title="country musics" alt="country musics"/>
-                
-                
-                
-                </div>
-            </div>
-            </div>
-        
-        
-        </div>
-        
-    </body>    
-    
-    
-    
-
-
+        <title>My Home Page</title>
+     </head>
+     <body>
+     <h1> Welcome to my website! I am Michael Alian</h1>
+     <ul>
+         <li><a href="index.php">HOME</a></li>
+         <li><a href="/GroupAct/MyWebpage.html">MyWebpage</a></li>
+    </ul>
+    </body>
 </html>
